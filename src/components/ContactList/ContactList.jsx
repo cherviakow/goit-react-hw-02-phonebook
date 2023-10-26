aaf453460653a76640e66256cf548ae2dae51938
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2023-10-26 11:38:44 -0400

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 const ContactList = ({ contacts, OnDelite }) => {
@@ -23,13 +22,3 @@ const ContactList = ({ contacts, OnDelite }) => {
   export default ContactList;
 
 
-ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        })
-    ),
-    onDelite: PropTypes.func.isRequired,
-};
